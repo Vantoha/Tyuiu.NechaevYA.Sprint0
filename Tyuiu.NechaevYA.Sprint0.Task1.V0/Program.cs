0738b3ace8ba9e17b3a8b7b957a20e9b061ad979
Vantoha <yaroslav7212@gmail.com>
2026-09-14 18:39:50 +0500

    Task1

---
+++ Tyuiu.NechaevYA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.NechaevYA.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Ярик";
+            int age = 20;
+            Console.WriteLine("Здраствуйте меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
