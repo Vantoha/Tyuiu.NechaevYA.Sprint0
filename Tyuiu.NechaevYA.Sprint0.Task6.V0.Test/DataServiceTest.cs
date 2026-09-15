4fed012e6743a411eb629b942d8d28dbef8276ab
Vantoha <yaroslav7212@gmail.com>
2026-09-16 00:55:46 +0500

    Task6

---
+++ Tyuiu.NechaevYA.Sprint0.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.NechaevYA.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.NechaevYA.Sprint0.Task6.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ChockAdditionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.AdditionArray(numbers);
+            Assert.AreEqual(15, res);
+        }
+            [TestMethod]
+            public void ChockSubstractionArrayValid()
+            {
+                var numbers = new int[] { 1, 2, 3, 4, 5 };
+                var res = DataService.SubtractionArray(numbers);
+                Assert.AreEqual(-15, res);
+            }
+        
+            [TestMethod]
+            public void ChockMultArrayValid()
+            {
+                var numbers = new int[] { 1, 2, 3, 4, 5 };
+                var res = DataService.MultiplicationArray(numbers);
+                Assert.AreEqual(120, res);
+            }
+        }
+    }
---
+++ Tyuiu.NechaevYA.Sprint0.Task6.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.NechaevYA.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.NechaevYA.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8fb46ed7-c992-48bc-806a-afde614f959e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
